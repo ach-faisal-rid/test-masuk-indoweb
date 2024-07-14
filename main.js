791f137037c325ef2f1eac840f35f200f1fcfaa1
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2024-07-14 09:15:22 +0700

    init 1

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById('pilihTahun').addEventListener('click', function() {
+    alert('Pilih Tahun clicked');
+});
+
+document.getElementById('tampilkan').addEventListener('click', function() {
+    alert('Tampilkan clicked');
+});
+
+document.getElementById('downloadDatabase').addEventListener('click', function() {
+    alert('Download Database clicked');
+});
